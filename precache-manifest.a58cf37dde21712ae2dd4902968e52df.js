1971c65893ef5bf27f5f03fb8f544124c4a6d4ff
simon duckett <simon.duckett@fmc.co.uk>
2019-10-10 19:52:59 +0100

    Updates

---
+++ precache-manifest.a58cf37dde21712ae2dd4902968e52df.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = [
+  {
+    "revision": "5aa54a47bb2833482887",
+    "url": "/static/css/main.8842f184.chunk.css"
+  },
+  {
+    "revision": "5aa54a47bb2833482887",
+    "url": "/static/js/main.5aa54a47.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "1b25e82af5b884173a53",
+    "url": "/static/css/2.3072dd8f.chunk.css"
+  },
+  {
+    "revision": "1b25e82af5b884173a53",
+    "url": "/static/js/2.1b25e82a.chunk.js"
+  },
+  {
+    "revision": "d74d87dd165fc5d52722f8357738a03d",
+    "url": "/static/media/AvenirLTStd-Black.d74d87dd.woff"
+  },
+  {
+    "revision": "46a0b7464e6186af6971adff3c489778",
+    "url": "/static/media/AvenirLTStd-Book.46a0b746.woff"
+  },
+  {
+    "revision": "c5833b37f54a60b621af207ee1748141",
+    "url": "/static/media/Gravity-Regular.c5833b37.otf"
+  },
+  {
+    "revision": "b997fee54ed6b4a9d397dcaee5b08bbb",
+    "url": "/static/media/AvenirLTStd-Heavy.b997fee5.woff"
+  },
+  {
+    "revision": "81cc224b6c5f572604d31566fbab6c0a",
+    "url": "/static/media/Gravity-Bold.81cc224b.otf"
+  },
+  {
+    "revision": "816b4ced446e252f881b78196a3f914c",
+    "url": "/static/media/beach.816b4ced.jpg"
+  },
+  {
+    "revision": "3575b89694a3f6a5eeb0582f02801dfa",
+    "url": "/static/media/lds.3575b896.png"
+  },
+  {
+    "revision": "c9836211d55c74f99e6e317fcbcf7632",
+    "url": "/static/media/artistbox.c9836211.png"
+  },
+  {
+    "revision": "979d19cbc2c7ba157f1c199403512108",
+    "url": "/static/media/IMG_1485.979d19cb.JPG"
+  },
+  {
+    "revision": "76046fb0bf6dcbb91c1bdf52fb1c5ffd",
+    "url": "/static/media/simonsbox.76046fb0.png"
+  },
+  {
+    "revision": "a9dd62e61342d2b2b462ff0dc4ad2631",
+    "url": "/static/media/dance.a9dd62e6.png"
+  },
+  {
+    "revision": "bb39a6de8bfa81dded00ae5a27033b4a",
+    "url": "/static/media/mystery.bb39a6de.png"
+  },
+  {
+    "revision": "e93717bb56f0a11437c92e539f721860",
+    "url": "/static/media/simonguitarbox.e93717bb.png"
+  },
+  {
+    "revision": "bca76834d72527856da70b4a2704d378",
+    "url": "/static/media/follow.bca76834.jpg"
+  },
+  {
+    "revision": "d3ce9564335d83e8aaa52e17958b34af",
+    "url": "/static/media/parade.d3ce9564.jpg"
+  },
+  {
+    "revision": "b74ec750e7e52ea328759c815ba383d2",
+    "url": "/static/media/ruos.b74ec750.jpg"
+  },
+  {
+    "revision": "48f52fc1f41edf122cf0d913dab42eb2",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/simon/precache-manifest.5df1fd83283663367ff318d16f4aebd5.js"
+  "/precache-manifest.a58cf37dde21712ae2dd4902968e52df.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/simon/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
